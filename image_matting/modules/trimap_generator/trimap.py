08a1e5c3f63f88dfdf17c35b21a64b51125b353a
gdgenchev <gdgenchev97@gmail.com>
2022-01-21 17:31:07 +0200

    Add option for specifying kernel size and iterations
    This will be needed when we use the module for training the alpha mask network, so we can choose random values and call the generate_trimap with them

--- image_matting/modules/trimap_generator/trimap.py
+++ image_matting/modules/trimap_generator/trimap.py
@@ -3,9 +3,9 @@ import numpy as np
 from cv2 import cv2
 
 
-def generate_trimap(image):
-    erosion = _erode(image)
-    dilation = _dilate(image)
+def generate_trimap(image, kernel_size, iterations):
+    erosion = _erode(image, kernel_size, iterations)
+    dilation = _dilate(image, kernel_size, iterations)
 
     trimap = np.full(image.shape, 128, dtype=np.uint8)
     trimap[erosion == 255] = 255
@@ -14,16 +14,16 @@ def generate_trimap(image):
     return trimap
 
 
-def _erode(image, iterations=10):
-    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
+def _erode(image, kernel_size, iterations):
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (kernel_size, kernel_size))
     image = cv2.erode(image, kernel, iterations=iterations)
     image[image > 0] = 255
 
     return image
 
 
-def _dilate(image, iterations=20):
-    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
+def _dilate(image, kernel_size, iterations):
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (kernel_size, kernel_size))
     image = cv2.dilate(image, kernel, iterations=iterations)
     image[image > 0] = 255
 
