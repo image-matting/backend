96e365952c78cfbf53e2abd3768c142c234ddc06
gdgenchev <gdgenchev97@gmail.com>
2022-01-26 21:12:17 +0200

    Refactoring

--- image_matting/modules/trimap_generator/trimap_generator_application.py
+++ image_matting/modules/trimap_generator/trimap_generator_application.py
@@ -6,6 +6,24 @@ from cv2 import cv2
 from trimap import generate_trimap
 
 
+def main():
+    args = parse_args()
+    image_path = args.image
+    output_directory_path = args.output
+
+    image_path = Path(image_path)
+    if not image_path.is_file():
+        raise RuntimeError(f'The provided image path "{image_path}" does not exist!')
+
+    image_filename = image_path.stem
+
+    image_path_str = image_path.as_posix()
+    image = cv2.imread(image_path_str, cv2.IMREAD_GRAYSCALE)
+    trimap_image = generate_trimap(image, kernel_size=5, iterations=15)
+
+    cv2.imwrite(f'{output_directory_path}/{image_filename}_trimap.png', trimap_image)
+
+
 def parse_args():
     parser = argparse.ArgumentParser(description='Trimap Generator Application')
     parser.add_argument('-i', '--image', required=True, type=str, help='path to input image')
@@ -14,13 +32,4 @@ def parse_args():
 
 
 if __name__ == "__main__":
-    args = parse_args()
-    image_path = args.image
-    output_directory_path = args.output
-
-    filename = Path(image_path).stem
-
-    image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
-    trimap_image = generate_trimap(image, kernel_size=5, iterations=15)
-
-    cv2.imwrite(f'{output_directory_path}/{filename}_trimap.png', trimap_image)
+    main()
